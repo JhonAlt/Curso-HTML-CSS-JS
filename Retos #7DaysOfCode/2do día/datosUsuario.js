04cf1fcc9171ebe129055fe1594380d094f5e575
Jonathan Amézquita <jamezquita150@gmail.com>
2025-02-26 23:16:36 -0600

    Mejorando las validaciones

--- Retos #7DaysOfCode/2do día/datosUsuario.js
+++ Retos #7DaysOfCode/2do día/datosUsuario.js
@@ -1,7 +1,21 @@
 
 let nombreUsuario = prompt("Cual es tu nombre?");
-let edadUsuario = parseInt(prompt("Cuantos años tienes?"));
+if (nombreUsuario === null || nombreUsuario === "") {
+    alert("Por favor, ingresa tu nombre.");
+    nombreUsuario = prompt("Cual es tu nombre?");
+}
+
+let edadUsuario = parseInt(prompt("Cuantos años tienes?")); 
+if (isNaN(edadUsuario) || edadUsuario <= 0) { 
+    alert("Por favor, ingresa un numero valido.");
+    edadUsuario = parseInt(prompt("Cuantos años tienes?"));
+}
+
 let lenguajeEstudio = prompt("Que lenguaje de programación estas estudiando?");
+if (lenguajeEstudio === null || lenguajeEstudio === "") {
+    alert("Por favor, ingresa el lenguaje de programación que estas estudiando.");
+    lenguajeEstudio = prompt("Que lenguaje de programación estas estudiando?");
+}
 
 alert(`Hola ${nombreUsuario}, tienes ${edadUsuario} años y estas aprendiendo ${lenguajeEstudio}.`); 
 
@@ -9,8 +23,10 @@ let respuesta = parseInt(prompt(`Te gusta estudiar ${lenguajeEstudio}? Responde 
 
 if (respuesta === 1){
     alert("Muy bien! Sigue estudiando y tendrás mucho éxito.");
-} else if (respuesta === 2){
-    alert("Oh, que pena... Ya intentaste aprender otros lenguajes?")
+} else if (respuesta === 2){ 
+    alert("Oh, que pena... Ya intentaste aprender otros lenguajes?") 
+} else {
+    alert("Por favor, ingresa una respuesta válida.");
+    respuesta = parseInt(prompt(`Te gusta estudiar ${lenguajeEstudio}? Responde con el numero 1 para SÍ o 2 para NO`));
 }
-
-
+    
